5f42781ed614d43cdf2fb0a9daa2b565b820bbc7
David Giraldo <davidgiraldobueno@gmail.com>
2021-06-18 12:02:42 -0500

    footer snapshot

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -11,5 +12,12 @@ describe('<Footer />', () => {
 
   test('Render of the title', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer snapshot', () => {
+  test('Check UI Footer component', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
