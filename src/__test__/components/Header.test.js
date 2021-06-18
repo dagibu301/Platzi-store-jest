c77f14872cdfb114f17c14dd42c0c593143a8d35
David Giraldo <davidgiraldobueno@gmail.com>
2021-06-18 12:10:24 -0500

    header snapshot

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/providerMock';
 import Header from '../../components/Header';
 
@@ -21,5 +22,16 @@ describe('<Header />', () => {
       </ProviderMock>,
     );
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header snapshot', () => {
+  test('Check UI Header component', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
